8933ad3b7b6c945f0e2db15d7080737b49cd157a
askarymrumhadi <askary10993@gmail.com>
2018-12-21 20:09:46 +0700

    add janitor check last activity on instances

--- __init__.py
+++ __init__.py
@@ -7,10 +7,11 @@ from aliyunsdkecs.request.v20140526 import StopInstanceRequest
 from credentials import ACCESS_ID, ACCESS_SECRET, REGION
 # Create an AcsClient instance
 client = AcsClient(ACCESS_ID, ACCESS_SECRET, REGION)
-
+'''
 # Create a request and set parameters
 request = DescribeInstancesRequest.DescribeInstancesRequest()
 request.set_PageSize(10)
 # Initiate an API request and display the response value
 response = client.do_action_with_exception(request)
 print response
+'''
