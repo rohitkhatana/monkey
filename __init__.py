a460ff5aae01b6eb194c65fb5a54599fd928482d
rohit khatana <rohitkhatana.khatana@gmail.com>
2018-12-21 16:05:21 +0700

    initialize the monkey project with simple alicloud integration

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkecs.request.v20140526 import DescribeInstancesRequest
+from aliyunsdkecs.request.v20140526 import StopInstanceRequest
+from credentials import ACCESS_ID, ACCESS_SECRET, REGION
+# Create an AcsClient instance
+client = AcsClient(ACCESS_ID, ACCESS_SECRET, REGION)
+
+# Create a request and set parameters
+request = DescribeInstancesRequest.DescribeInstancesRequest()
+request.set_PageSize(10)
+# Initiate an API request and display the response value
+response = client.do_action_with_exception(request)
+print response
