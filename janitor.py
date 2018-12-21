c9828fb05ad826cc76b5b666ce134257c23660af
Askary Muhammad <askary.muhammad@ovo.id>
2018-12-21 21:42:18 +0700

    refactor use dynamic commands to run ssh command

--- janitor.py
+++ janitor.py
@@ -9,11 +9,13 @@ class Janitor:
   def __init__(self, key, username):
     self.key = key
     self.username = username
-    self.commands = ["last -x"]
 
-  def check_last_activity(self):
+  def run_ssh_check(self, commands):
     instanceList = self.instances.get_instances()
     ipList = self.instances.get_ip_list()
+    
+    commandResult = ""
+    err = ""
 
     for ipAddr in ipList :
       key = paramiko.RSAKey.from_private_key_file(self.key)
@@ -21,9 +23,24 @@ class Janitor:
       sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
       sshClient.connect( hostname = ipAddr, username = self.username, pkey = key )
 
-      for command in self.commands:
+      for command in commands:
         stdin , stdout, stderr = sshClient.exec_command(command)
-        print stdout.read()
-        print stderr.read()
+        commandResult = stdout.read()
+        err = stderr.read()
 
       sshClient.close()
+
+    return commandResult, err
+
+  def set_commands(self, commands):
+    self.commands = commands
+
+  def get_logs_activity(self):
+    return self.run_ssh_check(["locate *.log"])
+
+  def get_last_activity(self):
+    return self.run_ssh_check(["last -x"])
+
+  def send_alert(self):
+    # implement alert here
+    pass
