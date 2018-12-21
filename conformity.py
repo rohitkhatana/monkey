711a2a3b364b7cdf70f58eb02f0088e17920d72c
rohit khatana <rohitkhatana.khatana@gmail.com>
2018-12-21 20:09:46 +0700

    auto_scaling check done

--- conformity.py
+++ conformity.py
@@ -1,14 +1,15 @@
 from client import client
 from auto_scaling import AutoScaling
-from instances import get_instances
+from instances import Instance
 
 class Conformity:
 
     def __init__(self):
         self._auto_scaling = AutoScaling()
+        self._instance = Instance()
 
-    def is_auto_scaline_enabled(self):
-        return self._auto_scaling.is_instance_in_auto_scale('asd')
+    def is_auto_scaling_enabled(self, instance):
+        return self._auto_scaling.is_instance_in_auto_scale(instance)
 
     def is_proper_tag(self):
         pass
@@ -19,11 +20,23 @@ class Conformity:
     def is_port_open_to_public(self):
         pass
 
-    def _get_instances(self):
-        return get_instances()
+    def __get_instances(self):
+        return self._instance.get_instances()
+
+    def __instances_id(self):
+        return map(lambda instance: instance['InstanceId'], self.__get_instances())
+
+    def __map_confirmity_status(self, instance_id):
+        return {
+            'is_auto_scaling_enabled': self.is_auto_scaling_enabled(instance_id),
+            'tag_status': '',
+            'is_monitoring_enabled': False,
+        }
 
     def perform(self):
-        instances = self._get_instances()
+        instances = self.__instances_id()
+        print map(lambda instance_id: self.__map_confirmity_status(instance_id), instances)
+
         print instances
 
 
--- instances.py
+++ instances.py
@@ -9,11 +9,13 @@ from aliyunsdkecs.request.v20140526 import StopInstanceRequest
 
 from client import client
 
-def get_instances():
-    # Create a request and set parameters
-    request = DescribeInstancesRequest.DescribeInstancesRequest()
-    request.set_PageSize(10)
-    # Initiate an API request and display the response value
-    response = client.do_action_with_exception(request)
-    response = json.loads(response)
-    return response['Instances']['Instance']
+class Instance:
+
+    def get_instances(self):
+        # Create a request and set parameters
+        request = DescribeInstancesRequest.DescribeInstancesRequest()
+        request.set_PageSize(10)
+        # Initiate an API request and display the response value
+        response = client.do_action_with_exception(request)
+        response = json.loads(response)
+        return response['Instances']['Instance']
