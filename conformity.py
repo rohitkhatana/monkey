4702ea1165019cf30b0ebf60cc7c7d6a34d6c18f
rohit khatana <rohitkhatana.khatana@gmail.com>
2018-12-25 16:38:26 +0700

    start using tag class for tag validation

--- conformity.py
+++ conformity.py
@@ -2,6 +2,7 @@ from client import client
 from auto_scaling import AutoScaling
 from instances import Instance
 from security_group import SecurityGroup
+from tag import Tag
 
 class Conformity:
 
@@ -9,45 +10,17 @@ class Conformity:
         self._auto_scaling = AutoScaling()
         self._instance = Instance()
         self._security_group = SecurityGroup()
+        self._tag = Tag()
         self.__valid_tags = ['env', 'serviceType', 'serviceName']
         self.__valid_env_tag_value = ['dev', 'stage', 'prod', 'uat']
 
     def is_auto_scaling_enabled(self, instance_id):
         return self._auto_scaling.is_instance_in_auto_scale(instance_id)
 
-    def __tag_display_msg(self, valid, msg):
-        return {'valid': valid, 'msg': msg}
-
-    def __valid_env_tag_value(self, tags):
-        env = filter(lambda tag: tag['TagKey'] == 'env', tags)
-        if len(env) == 1:
-            if env[0]['TagValue'] in self.__valid_env_tag_value:
-                return True
-            else:
-                return False
-        else:
-            False
-
-    def __find_missing_tags(self, tags):
-        valid = False
-        msgs = []
-        if not self.__valid_env_tag(tags):
-            msgs.append('invalid value for env tag or tag missing, valid value'.append(''.join(self.__valid_env_tag_value)))
-        missing = filter(lambda tag: tag['TagKey'] not in self.__valid_tags, tags)
-        #TODO: check for other tags value also
-        if len(missing) == 0:
-            return True, msgs
-        else:
-            return False, msgs
 
     def proper_tag(self, instance_id):
         tags = self._instance.tags(instance_id)
-
-        if len(tags) == 0:
-            return self.__tag_display_msg(False, 'missing environment, serviceName, serviceType tag')
-        else:
-            invalid, msgs = self.__find_missing_tags(tags)
-            return self.__tag_display_msg(invalid, ''.join(msgs))
+        return self._tag.validate_tags(tags)
 
     def is_monitor_enabled(self):
         pass
